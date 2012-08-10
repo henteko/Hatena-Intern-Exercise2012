46d2d0f2d7381d6d3208f0f4515d84a36214e82a
henteko <henteko07@gmail.com>
2012-08-10 15:43:03 +0900

    課題2-1 手直し

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,16 +1,12 @@
 var Template = function(input) {
-    this.tmp_html = input['source']; //自分に変数を設定
+    this.tmp_html = input.source; //自分に変数を設定
 };
 
 Template.prototype = {
     render: function(variables) {
-        //htmlエスケープ
-        for(key in variables) {
-            variables[key] = htmlEscape(variables[key]);
-        }
         //置換
         var out_html = this.tmp_html.replace(/{%\s*(\w+)\s*%}/g,function(){
-            return variables[RegExp.$1];
+            return vhtmlEscape(variables[RegExp.$1]);
         });
         //出力
         return out_html;
